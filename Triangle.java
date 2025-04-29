07991c8b63862039cd31c6b2c9b0417fd7decff9
iris <2436300@cegeplimoilou.ca>
2025-04-29 15:57:30 -0400

    implementation de Triangle (sauf equals)

--- Triangle.java
+++ Triangle.java
@@ -2,4 +2,33 @@ import java.util.*;
 
 public class Triangle {
     private List<Point> sommets;
+    private Set<Double> sideLengths;
+
+    public Triangle(ArrayList<Point> sommets) {
+        this.sommets = sommets;
+
+        setSideLengths();
+
+    }
+
+    private void setSideLengths() {
+        sideLengths = new HashSet<>();
+
+
+        sideLengths.add(
+                Math.sqrt(
+                        Math.pow(sommets.get(0).getX(), 2) + Math.pow(sommets.get(0).getY(), 2)
+                - Math.pow(sommets.get(1).getX(), 2) + Math.pow(sommets.get(1).getY(), 2))
+        );
+        sideLengths.add(
+                Math.sqrt(
+                        Math.pow(sommets.get(0).getX(), 2) + Math.pow(sommets.get(0).getY(), 2)
+                                - Math.pow(sommets.get(2).getX(), 2) + Math.pow(sommets.get(2).getY(), 2))
+        );
+        sideLengths.add(
+                Math.sqrt(
+                        Math.pow(sommets.get(2).getX(), 2) + Math.pow(sommets.get(1).getY(), 2)
+                                - Math.pow(sommets.get(1).getX(), 2) + Math.pow(sommets.get(1).getY(), 2))
+        );
+    }
 }
