f2131fb0f2750df206096d1e6266ad30dc7d08d2
2445147 <2445147@cegeplimoilou.ca>
2025-05-01 13:43:19 -0400

    ajout code pour: dotProduct, containsOrigin, touchesCenter, getSommet, setSideLenghts et equals

--- Triangle.java
+++ Triangle.java
@@ -1,14 +1,40 @@
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Triangle {
-    private List<Point> sommets;
+    private final List<Point> sommets;
+
+    private List<Double> xSommets;
+    private List<Double> ySommets;
+
     private Set<Double> sideLengths;
 
-    public Triangle(ArrayList<Point> sommets) {
-        this.sommets = sommets;
+    public Triangle(HashSet<Point> sommets) {
+        this.sommets = new ArrayList<>(sommets);
 
         setSideLengths();
 
+    }
+    public static double dotProduct(Point a, Point b) {
+        return a.getX() * b.getX() + a.getY() * b.getY();
+    }
+
+    public boolean containsOrigin(){
+        Point origine = new Point(0, 0);
+        if (Objects.equals(sommets.get(0), origine) || sommets.get(1).equals(origine) || sommets.get(2).equals(origine)) {
+            return false;
+        }
+        return ((!(dotProduct(sommets.get(0), sommets.get(1)) >= 0 && dotProduct(sommets.get(0), sommets.get(2)) >= 0 && dotProduct(sommets.get(2), sommets.get(1)) >= 0)) &&
+                (!(dotProduct(sommets.get(0), sommets.get(1)) <= 0 && dotProduct(sommets.get(0), sommets.get(2)) <= 0 && dotProduct(sommets.get(2), sommets.get(1)) <= 0))) &&
+                !touchesCenter2();
+    }
+
+    public boolean touchesCenter2(){
+        double angle0 = Math.atan(sommets.get(0).getX()/sommets.get(0).getY());
+        double angle1 = Math.atan(sommets.get(1).getX()/sommets.get(0).getY());
+        double angle2 = Math.atan(sommets.get(2).getX()/sommets.get(0).getY());
+        return (angle0 == -angle1 || angle0 == -angle2 || angle1 == -angle2);
     }
 
     public List<Point> getSommets() {
@@ -22,7 +48,7 @@ public class Triangle {
         sideLengths.add(
                 Math.sqrt(
                         Math.pow(sommets.get(0).getX(), 2) + Math.pow(sommets.get(0).getY(), 2)
-                - Math.pow(sommets.get(1).getX(), 2) + Math.pow(sommets.get(1).getY(), 2))
+                                - Math.pow(sommets.get(1).getX(), 2) + Math.pow(sommets.get(1).getY(), 2))
         );
         sideLengths.add(
                 Math.sqrt(
@@ -35,6 +61,10 @@ public class Triangle {
                                 - Math.pow(sommets.get(1).getX(), 2) + Math.pow(sommets.get(1).getY(), 2))
         );
     }
+
+
+
+
 
     @Override
     public boolean equals(Object o) {
